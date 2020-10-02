68bcdc65c351f2dbc60db4a058a760a1af130b73
Rozhkow <70947620+Rozhkow@users.noreply.github.com>
2020-10-02 15:11:20 +0300

    feat: add functipns code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+if(array === undefined) return 0;
+var len = array.length;
+if(len === 0) {
+ return 0;
+}
+var MIN = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (MIN > array[i]) MIN = array[i];
+    }
+    return MIN;
 }
 
 exports.max = function max (array) {
-  return 0;
+if(array === undefined) return 0;
+var len = array.length;
+if(len === 0) {
+ return 0;
+}
+var MAX = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (MAX < array[i]) MAX = array[i];
+    }
+    return MAX;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+if(array === undefined) return 0;
+var len = array.length;
+if(len === 0) {
+ return 0;
+}
+var sum = 0;
+var AVG = 0;
+var n = array.length;
+    for(var i = 0; i < array.length; i++){
+	sum += array[i];
+    }
+AVG = sum/n;
+  return AVG;
 }
